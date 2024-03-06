c621a9528530a2abc84bee8ab2930007f8b69d5c
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-03-06 11:26:52 -0300

    Atualizei

---
+++ assets/letras.js
@@ -0,0 +1,17 @@
+const elemento = document.querySelector('#texto');
+const texto = 'Sou um Desenvolvedor Front-End';
+const interval = 150;
+
+function showText(elemento, texto, interval) {
+    const char = texto.split('').reverse();
+    const typer = setInterval(() =>{
+        if(!char.length) {
+            return clearInterval(typer);
+        }
+        const next = char.pop();
+        elemento.innerHTML += next;
+
+    },interval) 
+}
+
+showText(elemento, texto, interval);
